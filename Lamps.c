79540cd7f68f44330ea76b5c544f9a37d29a137f
SriDurgaPrasad <119719304+SriDurgaPrasad@users.noreply.github.com>
2023-01-26 13:06:29 +0530

    #include<stdio.h>
    int main()
    {
        
        int n,k,x,y,u,uc,rc;
        scanf("%d %d %d %d",&n,&k,&x,&y);
        u=n-k;
        rc=k*x;
        uc=((rc+(u*y))>(rc+(u*x)))?(u*x):(u*y);
        printf("%d",rc+uc);
        
        return 0;
    }

---
+++ Lamps.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int n,k,x,y,u,uc,rc;
+    scanf("%d %d %d %d",&n,&k,&x,&y);
+    u=n-k;
+    rc=k*x;
+    uc=((rc+(u*y))>(rc+(u*x)))?(u*x):(u*y);
+    printf("%d",rc+uc);
+    
+    return 0;
+}
