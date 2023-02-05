3184e5265e13436354d9e9d61c7b6e5418a544c9
SriDurgaPrasad <119719304+SriDurgaPrasad@users.noreply.github.com>
2023-02-05 15:01:31 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     printf("1 2 3 4 5 6 7 8 9 10"); }

--- 1_to_10.c
+++ 1_to_10.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
+    int n;
     scanf("%d",&n);
-    for(i=1;i<=10;i++)
-    {
-        printf("%d ",i);
-    }
+    printf("1 2 3 4 5 6 7 8 9 10");
 }
